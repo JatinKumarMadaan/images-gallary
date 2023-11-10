da2fbbb82564c392d073e2cd022e905f2961e8c3
JatinKumarMadaan <jatinkmadaan@gmail.com>
2023-11-10 18:14:15 +0530

    sample flask app

---
+++ api/main.py
@@ -0,0 +1,18 @@
+# save this as app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+# def hello():
+#   return "Hello, World!"
+
+# app.route("/")(hello)
+
+
+
+@app.route("/")
+def hello():
+     return "Hello, World!"
+
+if __name__=="__main__":
+     app.run(host="0.0.0.0",port=5050)
