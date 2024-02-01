548c1083ab33fa4186ca321824cb3fac622a3d54
JatinKumarMadaan <jatinkmadaan@gmail.com>
2024-02-01 20:48:00 +0530

    single image endpoint with delete method enabled

--- api/main.py
+++ api/main.py
@@ -73,5 +73,31 @@ def imagas():
      # print(response.text)
      # return {"word": word}
 
+
+@app.route("/images/<image_id>",methods=["DELETE"])
+def image(image_id):
+     if request.method=="DELETE":
+       # delete image from the database
+          result=images_collection.delete_one({"_id":image_id})
+          if not result:
+               return {"error": "Image wasn't deleted. Please try again"}, 500
+          if result and not result.deleted_count:
+               return {"error": "Image not found"}, 404
+          return {"delete_id":image_id}
+          # image=request.get_json()
+          # if image["_id"]==image.get("id"): 
+          #      del_id=images_collection.delete_one(image_id)
+          
+          # deleted=images_collection.delete_one(image_id)
+          # del_id=deleted.del_id
+     
+     #   deleted_id=result.deleted_id
+     #   return {"deleted_id":deleted_id}
+     #   return {"deleted_id":del_id}
+     
+     # jsonify([img for img in images])
+
+
+
 if __name__=="__main__":
      app.run(host="0.0.0.0",port=5050,debug=True)
